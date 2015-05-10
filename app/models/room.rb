2f19e915db674dbd0970ea18f51710ecb6480540
German Escobar <german.escobarc@gmail.com>
2015-05-10 13:00:11 -0500

    Create model Room

---
+++ app/models/room.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: rooms
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  beds        :integer
+#  guests      :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Room < ActiveRecord::Base
+end
---
+++ db/migrate/20150510175923_create_rooms.rb
@@ -0,0 +1,12 @@
+class CreateRooms < ActiveRecord::Migration
+  def change
+    create_table :rooms do |t|
+      t.string :title
+      t.text :description
+      t.integer :beds
+      t.integer :guests
+
+      t.timestamps null: false
+    end
+  end
+end
