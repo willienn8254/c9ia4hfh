9a48e3c720dd7b6ede55f19e736f85109c7fbef4
willienn8254 <willienn@hotmail.com>
2016-01-28 21:32:55 -0500

    Valida modelo room y cambia idioma a español

--- Gemfile
+++ Gemfile
@@ -1,6 +1,11 @@
 source 'https://rubygems.org'
 
 
+gem 'tzinfo-data'
+gem 'tzinfo'
+gem 'coffee-script-source', '1.8.0'
+
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
 # Use sqlite3 as the database for Active Record
--- app/models/room.rb
+++ app/models/room.rb
@@ -13,4 +13,10 @@
 #
 
 class Room < ActiveRecord::Base
+
+	validates :title, presence: true
+	validates :description, presence: true, length: {maximum: 400}
+	validates :beds, presence: true, numericality: { only_integer: true }
+	validates :guests, presence: true, numericality: { only_integer: true }
+	validates :image_url, presence: true
 end
